8306764b41abf9506f7a6dda469a9b541306b405
Rasmus Kragh Wendelbo <rkw@svendborg-gym.dk>
2017-11-04 10:22:35 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+var learningRate=0.001;
+var epochs=100;
+points = [[0,0];[1,1];[3,7]];
+var a=1;
+var b=0;
+var y=0;
+var x=0;
+
 function setup() {
   createCanvas(1000,1000);
+  for (var i=0; i<epochs; i++) { 
+  for (var j=0; j<3; j++) {
+    y=a*points[j][0]+b;        
+    error=points[j][1]-y;
+    a=a+points[j][0]*error*learningRate;
+    b=b+error*learningRate;
+  }
+}
+print (a);
+print (b);
 }
 
 function draw() {
-  rect(50,50,30,30);
+  //rect(50,50,30,30);
 }
+
+
+
