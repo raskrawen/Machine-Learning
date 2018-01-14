4bc3449f79b1e9e7577b37657dc144783686ee5a
Rasmus Kragh Wendelbo <rkw@svendborg-gym.dk>
2018-01-14 22:03:47 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 var learningRate=0.001;
-var points = [[2,5],[1,1],[3,7]];
+var points = [[0,2],[2,5],[7,9]];
 var a=1;
 var b=0;
 var y=0;
 var x=0;
 var aSlider; //slider 
 var epochsOld=0;
-var epochs=100;
+var epochs=10;
 var learningRateOld = 0;
 var origoX = 50;
 var origoY = 500;
@@ -17,11 +17,12 @@ var buttonStart;
 var id;
 
   
-function setup() {
+function setup() { // runs once
   createCanvas(400,550);
-aSlider = createSlider(0,1000,100); // slider. Numbers from 0 to 1000, jump of 100.
+reset();
+aSlider = createSlider(0,100,10); // slider. Numbers from 0 to 100, jumps of 10, EPOCHS.
   aSlider.position(50,50);
-  bSlider = createSlider(0,100,10);
+  bSlider = createSlider(0,100,10); //learning rate * 1000
   bSlider.position(50,120);
 button1 = createButton ("reset");
 button1.position(200,10);
@@ -29,22 +30,25 @@ button1.mousePressed(reset);
 buttonStart = createButton ("train");
 buttonStart.position(200,50);
 buttonStart.mousePressed(start);
-  reset();
+  
   }
 
 function start() {
-  train(epochs);
+  epochs = aSlider.value();
+  learningRate = bSlider.value()/1000;
+  train(epochs,learningRate);
 }
 
 function reset() {
   clear();
-  a=1;
-  b=0;
-  learningRate=0.01;
-  epochs=100;
+a=1;
+b=0;
+//  learningRate=0.01;
+//  epochs=10;
   writer();
   drawPoints(points,origoX, origoY);
   drawLine(a,b,origoX, origoY);
+  clearInterval(id);
 }
 
 function drawPoints(points1,origoX, origoY) {
@@ -74,25 +78,22 @@ function drawLine(a,b,origoX, origoY) {
   x=map(x,0,10,0,200,true);
  }
  else {
-  var x=200;
+  var x=200; //x at max, x=10= 200 px
   y=a*10+b;
-  y=map(y,0,10,0,200);
+  y=map(y,0,10,0,200); //map y to pixels
  }
   //draw line:
-  var onePoint = [x+origoX,origoY-y];
+  var onePoint = [x+origoX,origoY-y]; // point on line.
   append(pointsInLine, onePoint);
   //fill(204, 101, 192, 127);
   stroke(0);
-  line(pointsInLine[0][0],pointsInLine[0][1],pointsInLine[1][0],pointsInLine[1][1]);
- 
+  line(pointsInLine[0][0],pointsInLine[0][1],pointsInLine[1][0],pointsInLine[1][1]); // drawinf the line
 }
 
-function train(epochs) {
-  epochs = aSlider.value();
-  learningRate = bSlider.value()/1000;
-  writer();
-  epoch=0; 
-  id= setInterval(oneTraining,1000);
+
+function train(epochs,learningRate) {
+    //epoch=0; 
+  id= setInterval(oneTraining,500); //time between each EPOCH
 }
 
 
@@ -105,14 +106,16 @@ function oneTraining() {
     b=b+error*learningRate;
     }
   drawLine (a,b,origoX,origoY);
+  writer();
   if (epoch>=epochs) { //when number of epochs exceeds epochs target: stop
     clearInterval(id);
+    epoch=0;
   }
 }
 
 
 function writer() {
-  clearTextArea(); 
+  clearTextArea(); //function which clears text
   noFill();
   rect(50,300,200,200); //coordinate system
   fill(0);
@@ -124,7 +127,8 @@ function writer() {
    text("a: " + a, 50, 200);
 text("b:" + b, 50, 220);
 text("epoch: " + epoch, 50, 240);
-text("learning rate:" + learningRate, 50, 260);
+text("epochs: " + epochs, 50, 260);
+text("learning rate:" + learningRate, 50, 280);
 
 }
 
